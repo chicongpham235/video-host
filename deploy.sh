fd3d554d84dddc88e8c0d7d3a492a47aa5cdba05
chicongpham235 <chicongpham235@gmail.com>
2023-02-12 16:39:32 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 git init 
 git add -A
 git commit -m "deploy"
-git push -f https://github.com/chicongpham235/video-host.git master:gh-pages
+git push -f https://github.com/chicongpham235/video-test.git master:gh-pages
