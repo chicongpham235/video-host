91d4fa6c3758c407341423f2bf0477d3b350eddb
chicongpham235 <chicongpham235@gmail.com>
2023-02-12 22:51:59 +0700

    deploy

--- js/index.js
+++ js/index.js
@@ -155,7 +155,7 @@ async function updateToggleControl() {
     isPaused = false;
     faceMesh.onResults(findFaceMesh);
   }
-  console.log(camera.g.id);
+  console.log(camera.g?.id);
   if (onCamera) {
     capture.setAttribute("data-title", "Hide Camera");
     cameraElement.style.display = "inline";
